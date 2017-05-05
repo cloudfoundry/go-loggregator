32b70e46515ecbb2d05e54ce7168bd86efea9594
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-05-05 10:48:08 -0600

    Remove unused generated code
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- loggregator_v2/ingress.pb.go
+++ loggregator_v2/ingress.pb.go
@@ -2,6 +2,25 @@
 // source: ingress.proto
 // DO NOT EDIT!
 
+/*
+Package loggregator_v2 is a generated protocol buffer package.
+
+It is generated from these files:
+	ingress.proto
+	envelope.proto
+
+It has these top-level messages:
+	IngressResponse
+	EnvelopeBatch
+	BatchSenderResponse
+	Envelope
+	Value
+	Log
+	Counter
+	Gauge
+	GaugeValue
+	Timer
+*/
 package loggregator_v2
 
 import proto "github.com/golang/protobuf/proto"
@@ -18,13 +37,19 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+
 type IngressResponse struct {
 }
 
 func (m *IngressResponse) Reset()                    { *m = IngressResponse{} }
 func (m *IngressResponse) String() string            { return proto.CompactTextString(m) }
 func (*IngressResponse) ProtoMessage()               {}
-func (*IngressResponse) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{0} }
+func (*IngressResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
 type EnvelopeBatch struct {
 	Batch []*Envelope `protobuf:"bytes,1,rep,name=batch" json:"batch,omitempty"`
@@ -33,7 +58,7 @@ type EnvelopeBatch struct {
 func (m *EnvelopeBatch) Reset()                    { *m = EnvelopeBatch{} }
 func (m *EnvelopeBatch) String() string            { return proto.CompactTextString(m) }
 func (*EnvelopeBatch) ProtoMessage()               {}
-func (*EnvelopeBatch) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{1} }
+func (*EnvelopeBatch) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 func (m *EnvelopeBatch) GetBatch() []*Envelope {
 	if m != nil {
@@ -48,7 +73,7 @@ type BatchSenderResponse struct {
 func (m *BatchSenderResponse) Reset()                    { *m = BatchSenderResponse{} }
 func (m *BatchSenderResponse) String() string            { return proto.CompactTextString(m) }
 func (*BatchSenderResponse) ProtoMessage()               {}
-func (*BatchSenderResponse) Descriptor() ([]byte, []int) { return fileDescriptor2, []int{2} }
+func (*BatchSenderResponse) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 func init() {
 	proto.RegisterType((*IngressResponse)(nil), "loggregator.v2.IngressResponse")
@@ -229,9 +254,9 @@ var _Ingress_serviceDesc = grpc.ServiceDesc{
 	Metadata: "ingress.proto",
 }
 
-func init() { proto.RegisterFile("ingress.proto", fileDescriptor2) }
+func init() { proto.RegisterFile("ingress.proto", fileDescriptor0) }
 
-var fileDescriptor2 = []byte{
+var fileDescriptor0 = []byte{
 	// 185 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0xcd, 0xcc, 0x4b, 0x2f,
 	0x4a, 0x2d, 0x2e, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0xcb, 0xc9, 0x4f, 0x4f, 0x2f,
--- loggregator_v2/scripts/generate_protos.sh
+++ loggregator_v2/scripts/generate_protos.sh
@@ -6,4 +6,5 @@ go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 
 protoc --proto_path=$LOGGREGATOR_API/v2 \
        --go_out=plugins=grpc:. \
-       $LOGGREGATOR_API/v2/*.proto
+       $LOGGREGATOR_API/v2/ingress.proto \
+       $LOGGREGATOR_API/v2/envelope.proto
