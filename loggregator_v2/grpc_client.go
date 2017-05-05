f09d63f40f19eb888a26468c58ce07afcaaa6d2d
Warren Fernandes <warren.f.fernandes@gmail.com>
2017-05-05 10:05:42 -0600

    Relocates grpc batcher into separate file
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- loggregator_v2/grpc_client.go
+++ loggregator_v2/grpc_client.go
@@ -117,62 +117,6 @@ func (c *grpcClient) send(envelope *Envelope) error {
 	return err
 }
 
-type grpcBatcher struct {
-	c       *grpcClient
-	metrics map[string]*GaugeValue
-}
-
-func (gb *grpcBatcher) Send() error {
-	return gb.c.send(&Envelope{
-		Timestamp: time.Now().UnixNano(),
-		Message: &Envelope_Gauge{
-			Gauge: &Gauge{
-				Metrics: gb.metrics,
-			},
-		},
-	})
-}
-
-func (c *grpcBatcher) SendDuration(name string, duration time.Duration) error {
-	c.metrics[name] = &GaugeValue{
-		Unit:  "nanos",
-		Value: float64(duration),
-	}
-	return nil
-}
-
-func (c *grpcBatcher) SendMebiBytes(name string, mebibytes int) error {
-	c.metrics[name] = &GaugeValue{
-		Unit:  "MiB",
-		Value: float64(mebibytes),
-	}
-	return nil
-}
-
-func (c *grpcBatcher) SendMetric(name string, value int) error {
-	c.metrics[name] = &GaugeValue{
-		Unit:  "Metric",
-		Value: float64(value),
-	}
-	return nil
-}
-
-func (c *grpcBatcher) SendBytesPerSecond(name string, value float64) error {
-	c.metrics[name] = &GaugeValue{
-		Unit:  "B/s",
-		Value: value,
-	}
-	return nil
-}
-
-func (c *grpcBatcher) SendRequestsPerSecond(name string, value float64) error {
-	c.metrics[name] = &GaugeValue{
-		Unit:  "Req/s",
-		Value: value,
-	}
-	return nil
-}
-
 func (c *grpcClient) Batcher() Batcher {
 	return &grpcBatcher{
 		c:       c,
