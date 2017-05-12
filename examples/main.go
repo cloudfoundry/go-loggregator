641f339c91e889d7a930e6cde3a6f5d2d4628b62
Warren Fernandes <wfernandes@pivotal.io>
2017-05-12 16:29:04 -0600

    Update example
    Signed-off-by: Andrew Poydence <apoydence@pivotal.io>

--- examples/main.go
+++ examples/main.go
@@ -4,28 +4,27 @@ import (
 	"log"
 	"os"
 
-	loggregator "code.cloudfoundry.org/go-loggregator"
+	"code.cloudfoundry.org/go-loggregator/v2"
 )
 
 func main() {
-	metronCfg := loggregator.Config{
-		UseV2API:      true,
-		APIPort:       3458,
-		CACertPath:    os.Getenv("CA_CERT_PATH"),
-		CertPath:      os.Getenv("CERT_PATH"),
-		KeyPath:       os.Getenv("KEY_PATH"),
-		JobDeployment: "example-deployment",
-		JobName:       "example-job",
-		JobIndex:      "example-index",
-		JobIP:         "0.0.0.0",
-		JobOrigin:     "example-deployment",
+	tlsConfig, err := v2.NewTLSConfig(
+		os.Getenv("CA_CERT_PATH"),
+		os.Getenv("CERT_PATH"),
+		os.Getenv("KEY_PATH"),
+	)
+	if err != nil {
+		log.Fatal("Could not create TLS config", err)
 	}
 
-	client, err := loggregator.NewClient(metronCfg)
+	client, err := v2.NewClient(
+		tlsConfig,
+		v2.WithPort(3458),
+	)
 
 	if err != nil {
 		log.Fatal("Could not create client", err)
 	}
 
-	client.SendMetric("some-metric-name", 1234)
+	client.EmitLog("some log goes here")
 }
