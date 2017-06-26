d07b3ac23b0385ee2c2c4144d45983046252dfa1
Andrew Poydence <apoydence@pivotal.io>
2017-06-26 09:48:17 -0600

    Updated function comments

--- tls.go
+++ tls.go
@@ -7,14 +7,14 @@ import (
 	"io/ioutil"
 )
 
-// NewTLSConfig provides a convenient means for creating a *tls.Config
-// which uses the CA, cert, and key.
+// NewIngressTLSConfig provides a convenient means for creating a *tls.Config
+// which uses the CA, cert, and key for the ingress endpoint.
 func NewIngressTLSConfig(caPath, certPath, keyPath string) (*tls.Config, error) {
 	return newTLSConfig(caPath, certPath, keyPath, "metron")
 }
 
-// NewTLSConfig provides a convenient means for creating a *tls.Config
-// which uses the CA, cert, and key.
+// NewEgressTLSConfig provides a convenient means for creating a *tls.Config
+// which uses the CA, cert, and key for the egress endpoint.
 func NewEgressTLSConfig(caPath, certPath, keyPath string) (*tls.Config, error) {
 	return newTLSConfig(caPath, certPath, keyPath, "reverselogproxy")
 }
