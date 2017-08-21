434a1d423be2fb0e56214bc73a967dc139f06025
Eno Compton <eno4@ecom.com>
2017-08-21 16:50:03 -0600

    Minor documentation update

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
-// Package v2 provides a client to send data to the Loggregator v2 API.
+// Package loggregator provides clients to send data to the Loggregator v1 and
+// v2 API.
 //
 // The v2 API distinguishes itself from the v1 API on three counts:
 //
@@ -6,9 +7,9 @@
 // 2) it uses a streaming connection, and
 // 3) it supports batching to improve performance.
 //
-// The code here provides a generic interface into the v2 API. Clients who
-// prefer more fine grained control may generate their own code using
-// the protobuf and gRPC service definitions found at:
+// The code here provides a generic interface into the two APIs. Clients who
+// prefer more fine grained control may generate their own code using the
+// protobuf and gRPC service definitions found at:
 // github.com/cloudfoundry/loggregator-api.
 //
 // Note that on account of the client using batching wherein multiple
