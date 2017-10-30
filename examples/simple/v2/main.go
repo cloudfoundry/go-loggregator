6641d989fc5fd4ef4457a869d3875b3bb1e189c7
Brady Love <blove@pivotal.io>
2017-10-30 14:31:11 -0600

    Update example v2 client to emit event before logs
    [#151851103]
    
    Signed-off-by: Johanna Smith <josmith@pivotal.io>

--- examples/simple/v2/main.go
+++ examples/simple/v2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
@@ -27,9 +28,19 @@ func main() {
 		log.Fatal("Could not create client", err)
 	}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = client.EmitEvent(
+		ctx,
+		"Starting sample V2 Client",
+		"This sample V2 client is about to emit 50 log envelopes",
+	)
+	if err != nil {
+		log.Fatalf("Failed to emit event: %s", err)
+	}
+
 	for i := 0; i < 50; i++ {
 		client.EmitLog("some log goes here")
-		client.EmitEvent("event-title", "event-body")
 
 		time.Sleep(10 * time.Millisecond)
 	}
