44139f192b32a72c5a2884d5b3ad91d40d22f001
Brady Love <blove@pivotal.io>
2018-09-05 16:08:29 -0600

    Add Increment and Decrement to pulseemitter GaugeMetric
    [#155086265]

--- pulseemitter/gauge_metric_test.go
+++ pulseemitter/gauge_metric_test.go
@@ -9,10 +9,7 @@ import (
 
 var _ = Describe("GaugeMetric", func() {
 	It("prepares the envelope for delivery", func() {
-		g := pulseemitter.NewGaugeMetric(
-			"some-gauge",
-			"some-unit",
-			"my-source-id",
+		g := pulseemitter.NewGaugeMetric("some-gauge", "some-unit", "my-source-id",
 			pulseemitter.WithVersion(1, 2),
 		)
 
@@ -21,15 +18,7 @@ var _ = Describe("GaugeMetric", func() {
 		spy := newSpyLogClient()
 		g.Emit(spy)
 
-		e := &loggregator_v2.Envelope{
-			Message: &loggregator_v2.Envelope_Gauge{
-				Gauge: &loggregator_v2.Gauge{
-					Metrics: make(map[string]*loggregator_v2.GaugeValue),
-				},
-			},
-			Tags: make(map[string]string),
-		}
-
+		e := buildBaseGauge()
 		for _, o := range spy.GaugeOpts() {
 			o(e)
 		}
@@ -41,4 +30,49 @@ var _ = Describe("GaugeMetric", func() {
 		Expect(e.GetTags()).To(HaveKey("metric_version"))
 		Expect(e.GetTags()["metric_version"]).To(Equal("1.2"))
 	})
+
+	It("increments the gauge value", func() {
+		g := pulseemitter.NewGaugeMetric("some-gauge", "some-unit", "my-source-id",
+			pulseemitter.WithVersion(1, 2),
+		)
+		g.Set(10.21)
+		g.Increment(10.21)
+
+		spy := newSpyLogClient()
+		g.Emit(spy)
+
+		e := buildBaseGauge()
+		for _, o := range spy.GaugeOpts() {
+			o(e)
+		}
+		Expect(e.GetGauge().GetMetrics()["some-gauge"].GetValue()).To(Equal(20.42))
+	})
+
+	It("decrements the gauge value", func() {
+		g := pulseemitter.NewGaugeMetric("some-gauge", "some-unit", "my-source-id",
+			pulseemitter.WithVersion(1, 2),
+		)
+		g.Set(10.21)
+		g.Decrement(5)
+
+		spy := newSpyLogClient()
+		g.Emit(spy)
+
+		e := buildBaseGauge()
+		for _, o := range spy.GaugeOpts() {
+			o(e)
+		}
+		Expect(e.GetGauge().GetMetrics()["some-gauge"].GetValue()).To(Equal(5.21))
+	})
 })
+
+func buildBaseGauge() *loggregator_v2.Envelope {
+	return &loggregator_v2.Envelope{
+		Message: &loggregator_v2.Envelope_Gauge{
+			Gauge: &loggregator_v2.Gauge{
+				Metrics: make(map[string]*loggregator_v2.GaugeValue),
+			},
+		},
+		Tags: make(map[string]string),
+	}
+}
