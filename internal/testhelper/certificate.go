ec2ec210433b1097be202ea4768da585900e8206
Joris Baum <joris.baum@sap.com>
2026-02-23 15:20:20 +0100

    fix: Remove bindata, use cloudfoundry/tlsconfig

---
+++ internal/testhelper/certificate.go
@@ -0,0 +1,123 @@
+package testhelper
+
+import (
+	"log"
+	"os"
+
+	"code.cloudfoundry.org/tlsconfig/certtest"
+)
+
+type TestCerts struct {
+	ca *certtest.Authority
+
+	caFile       string
+	certKeyPairs map[string]certKeyPair
+}
+
+type certKeyPair struct {
+	certFile string
+	keyFile  string
+}
+
+func (tc *TestCerts) CA() string {
+	return tc.caFile
+}
+
+func (tc *TestCerts) Cert(commonName string) string {
+	return tc.keyPair(commonName).certFile
+}
+
+func (tc *TestCerts) Key(commonName string) string {
+	return tc.keyPair(commonName).keyFile
+}
+
+func (tc *TestCerts) keyPair(commonName string) certKeyPair {
+	keyPair, ok := tc.certKeyPairs[commonName]
+	if !ok {
+		keyPair = tc.generateCertKeyPair(commonName)
+		tc.certKeyPairs[commonName] = keyPair
+	}
+
+	return keyPair
+}
+
+func LoadCertsFromEnv() *TestCerts {
+	caFile := os.Getenv("TEST_CA_FILE")
+	certFile := os.Getenv("TEST_CERT_FILE")
+	keyFile := os.Getenv("TEST_KEY_FILE")
+
+	if caFile == "" || certFile == "" || keyFile == "" {
+		log.Fatal("TEST_CA_FILE, TEST_CERT_FILE, and TEST_KEY_FILE must be set")
+	}
+
+	return &TestCerts{
+		caFile: caFile,
+		certKeyPairs: map[string]certKeyPair{
+			"metron": {
+				certFile: certFile,
+				keyFile:  keyFile,
+			},
+		},
+	}
+}
+
+func GenerateCerts(caName string) *TestCerts {
+	ca, caFile := generateCA(caName)
+
+	return &TestCerts{
+		ca:           ca,
+		caFile:       caFile,
+		certKeyPairs: map[string]certKeyPair{},
+	}
+}
+
+func generateCA(caName string) (*certtest.Authority, string) {
+	ca, err := certtest.BuildCA(caName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caBytes, err := ca.CertificatePEM()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileName := tmpFile(caName+".crt", caBytes)
+
+	return ca, fileName
+}
+
+func tmpFile(prefix string, caBytes []byte) string {
+	file, err := os.CreateTemp("", prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(caBytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return file.Name()
+}
+
+func (tc *TestCerts) generateCertKeyPair(commonName string) certKeyPair {
+	cert, err := tc.ca.BuildSignedCertificate(commonName, certtest.WithDomains(commonName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	certBytes, keyBytes, err := cert.CertificatePEMAndPrivateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	certFile := tmpFile(commonName+".crt", certBytes)
+	keyFile := tmpFile(commonName+".key", keyBytes)
+
+	return certKeyPair{
+		certFile: certFile,
+		keyFile:  keyFile,
+	}
+}
