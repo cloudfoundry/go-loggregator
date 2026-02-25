cec720137ab7d4e6b6fdab76f955a6dbafbb9f33
Joris Baum <joris.baum@sap.com>
2026-02-25 11:03:39 +0100

    Migrate tests to Ginkgo

--- rfc5424/message_test.go
+++ rfc5424/message_test.go
@@ -1,80 +1,81 @@
-package rfc5424
+package rfc5424_test
 
-import . "gopkg.in/check.v1"
+import (
+	"code.cloudfoundry.org/go-loggregator/v10/rfc5424"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
 
-var _ = Suite(&MessageTest{})
-
-type MessageTest struct {
-}
-
-func (s *MessageTest) TestAddDatum(c *C) {
-	m := Message{}
-	m.AddDatum("id", "name", "value")
-	c.Assert(m, DeepEquals, Message{
-		StructuredData: []StructuredData{
-			{
-				ID: "id",
-				Parameters: []SDParam{
-					{"name", "value"},
+var _ = Describe("RFC5424 Message", func() {
+	It("adds datum to a message", func() {
+		m := rfc5424.Message{}
+		m.AddDatum("id", "name", "value")
+		Expect(m).To(Equal(rfc5424.Message{
+			StructuredData: []rfc5424.StructuredData{
+				{
+					ID: "id",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+					},
 				},
 			},
-		},
+		}))
+
+		m.AddDatum("id2", "name", "value")
+		Expect(m).To(Equal(rfc5424.Message{
+			StructuredData: []rfc5424.StructuredData{
+				{
+					ID: "id",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+					},
+				},
+				{
+					ID: "id2",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+					},
+				},
+			},
+		}))
+
+		m.AddDatum("id", "name2", "value2")
+		Expect(m).To(Equal(rfc5424.Message{
+			StructuredData: []rfc5424.StructuredData{
+				{
+					ID: "id",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+						{"name2", "value2"},
+					},
+				},
+				{
+					ID: "id2",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+					},
+				},
+			},
+		}))
+
+		m.AddDatum("id", "name", "value3")
+		Expect(m).To(Equal(rfc5424.Message{
+			StructuredData: []rfc5424.StructuredData{
+				{
+					ID: "id",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+						{"name2", "value2"},
+						{"name", "value3"},
+					},
+				},
+				{
+					ID: "id2",
+					Parameters: []rfc5424.SDParam{
+						{"name", "value"},
+					},
+				},
+			},
+		}))
 	})
-
-	m.AddDatum("id2", "name", "value")
-	c.Assert(m, DeepEquals, Message{
-		StructuredData: []StructuredData{
-			{
-				ID: "id",
-				Parameters: []SDParam{
-					{"name", "value"},
-				},
-			},
-			{
-				ID: "id2",
-				Parameters: []SDParam{
-					{"name", "value"},
-				},
-			},
-		},
-	})
-
-	m.AddDatum("id", "name2", "value2")
-	c.Assert(m, DeepEquals, Message{
-		StructuredData: []StructuredData{
-			{
-				ID: "id",
-				Parameters: []SDParam{
-					{"name", "value"},
-					{"name2", "value2"},
-				},
-			},
-			{
-				ID: "id2",
-				Parameters: []SDParam{
-					{"name", "value"},
-				},
-			},
-		},
-	})
-
-	m.AddDatum("id", "name", "value3")
-	c.Assert(m, DeepEquals, Message{
-		StructuredData: []StructuredData{
-			{
-				ID: "id",
-				Parameters: []SDParam{
-					{"name", "value"},
-					{"name2", "value2"},
-					{"name", "value3"},
-				},
-			},
-			{
-				ID: "id2",
-				Parameters: []SDParam{
-					{"name", "value"},
-				},
-			},
-		},
-	})
-}
+})
--- rfc5424/stream_test.go
+++ rfc5424/stream_test.go
@@ -1,84 +1,89 @@
-package rfc5424
+package rfc5424_test
 
 import (
 	"bytes"
 
-	. "gopkg.in/check.v1"
+	"code.cloudfoundry.org/go-loggregator/v10/rfc5424"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-var _ = Suite(&StreamTest{})
+var _ = Describe("RFC5424 Stream", func() {
+	It("can read and write messages", func() {
+		stream := bytes.Buffer{}
+		for i := 0; i < 4; i++ {
+			m := rfc5424.Message{Priority: rfc5424.Priority(i), Timestamp: T("2003-08-24T05:14:15.000003-07:00")}
+			nbytes, err := m.WriteTo(&stream)
+			Expect(err).To(BeNil())
+			Expect(nbytes).To(Equal(int64(50)))
+		}
 
-type StreamTest struct {
-}
+		Expect(stream.String()).To(Equal(
+			`47 <0>1 2003-08-24T05:14:15.000003-07:00 - - - - -` +
+				`47 <1>1 2003-08-24T05:14:15.000003-07:00 - - - - -` +
+				`47 <2>1 2003-08-24T05:14:15.000003-07:00 - - - - -` +
+				`47 <3>1 2003-08-24T05:14:15.000003-07:00 - - - - -`,
+		))
 
-func (s *StreamTest) TestCanReadAndWrite(c *C) {
-	stream := bytes.Buffer{}
-	for i := 0; i < 4; i++ {
-		m := Message{Priority: Priority(i), Timestamp: T("2003-08-24T05:14:15.000003-07:00")}
-		nbytes, err := m.WriteTo(&stream)
-		c.Assert(err, IsNil)
-		c.Assert(nbytes, Equals, int64(50))
-	}
+		for i := 0; i < 4; i++ {
+			m := rfc5424.Message{Priority: rfc5424.Priority(i << 3)}
+			nbytes, err := m.ReadFrom(&stream)
+			Expect(err).To(BeNil())
+			Expect(nbytes).To(Equal(int64(50)))
+			Expect(m).To(Equal(rfc5424.Message{
+				Priority:       rfc5424.Priority(i),
+				Timestamp:      T("2003-08-24T05:14:15.000003-07:00"),
+				StructuredData: []rfc5424.StructuredData{}},
+			))
+		}
+	})
 
-	c.Assert(stream.String(), Equals,
-		`47 <0>1 2003-08-24T05:14:15.000003-07:00 - - - - -`+
-			`47 <1>1 2003-08-24T05:14:15.000003-07:00 - - - - -`+
-			`47 <2>1 2003-08-24T05:14:15.000003-07:00 - - - - -`+
-			`47 <3>1 2003-08-24T05:14:15.000003-07:00 - - - - -`)
+	It("can use UTC Timestamps", func() {
+		stream := bytes.Buffer{}
+		for i := 0; i < 4; i++ {
+			m := rfc5424.Message{Priority: rfc5424.Priority(i), Timestamp: T("2003-08-24T05:14:15.000003+00:00"), UseUTC: true}
+			nbytes, err := m.WriteTo(&stream)
+			Expect(err).To(BeNil())
+			Expect(nbytes).To(Equal(int64(45)))
+		}
 
-	for i := 0; i < 4; i++ {
-		m := Message{Priority: Priority(i << 3)}
-		nbytes, err := m.ReadFrom(&stream)
-		c.Assert(err, IsNil)
-		c.Assert(nbytes, Equals, int64(50))
-		c.Assert(m, DeepEquals, Message{Priority: Priority(i),
-			Timestamp:      T("2003-08-24T05:14:15.000003-07:00"),
-			StructuredData: []StructuredData{}})
-	}
-}
+		Expect(stream.String()).To(Equal(
+			`42 <0>1 2003-08-24T05:14:15.000003Z - - - - -` +
+				`42 <1>1 2003-08-24T05:14:15.000003Z - - - - -` +
+				`42 <2>1 2003-08-24T05:14:15.000003Z - - - - -` +
+				`42 <3>1 2003-08-24T05:14:15.000003Z - - - - -`,
+		))
 
-func (s *StreamTest) TestUtcTimestamps(c *C) {
-	stream := bytes.Buffer{}
-	for i := 0; i < 4; i++ {
-		m := Message{Priority: Priority(i), Timestamp: T("2003-08-24T05:14:15.000003+00:00"), UseUTC: true}
-		nbytes, err := m.WriteTo(&stream)
-		c.Assert(err, IsNil)
-		c.Assert(nbytes, Equals, int64(45))
-	}
+		for i := 0; i < 4; i++ {
+			m := rfc5424.Message{Priority: rfc5424.Priority(i << 3)}
+			nbytes, err := m.ReadFrom(&stream)
+			Expect(err).To(BeNil())
+			Expect(nbytes).To(Equal(int64(45)))
 
-	c.Assert(stream.String(), Equals,
-		`42 <0>1 2003-08-24T05:14:15.000003Z - - - - -`+
-			`42 <1>1 2003-08-24T05:14:15.000003Z - - - - -`+
-			`42 <2>1 2003-08-24T05:14:15.000003Z - - - - -`+
-			`42 <3>1 2003-08-24T05:14:15.000003Z - - - - -`)
+			Expect(m).To(Equal(rfc5424.Message{
+				Priority:       rfc5424.Priority(i),
+				Timestamp:      UTC("2003-08-24T05:14:15.000003Z"),
+				StructuredData: []rfc5424.StructuredData{},
+				UseUTC:         true,
+			}))
+		}
+	})
 
-	for i := 0; i < 4; i++ {
-		m := Message{Priority: Priority(i << 3)}
-		nbytes, err := m.ReadFrom(&stream)
-		c.Assert(err, IsNil)
-		c.Assert(nbytes, Equals, int64(45))
-		c.Assert(m, DeepEquals, Message{Priority: Priority(i),
-			Timestamp:      UTC("2003-08-24T05:14:15.000003Z"),
-			StructuredData: []StructuredData{},
-			UseUTC:         true,
-		})
-	}
-}
+	It("rejects invalid streams", func() {
+		stream := bytes.NewBufferString(`99 <0>1 2003-08-24T05:14:15.000003-07:00 - - - - -`)
+		for i := 0; i < 4; i++ {
+			m := rfc5424.Message{Priority: rfc5424.Priority(i << 3)}
+			_, err := m.ReadFrom(stream)
+			Expect(err).NotTo(BeNil())
+		}
+	})
 
-func (s *StreamTest) TestRejectsInvalidStream(c *C) {
-	stream := bytes.NewBufferString(`99 <0>1 2003-08-24T05:14:15.000003-07:00 - - - - -`)
-	for i := 0; i < 4; i++ {
-		m := Message{Priority: Priority(i << 3)}
-		_, err := m.ReadFrom(stream)
-		c.Assert(err, Not(IsNil))
-	}
-}
-
-func (s *StreamTest) TestRejectsInvalidStream2(c *C) {
-	stream := bytes.NewBufferString(`0 <0>1 2003-08-24T05:14:15.000003-07:00 - - - - -`)
-	for i := 0; i < 4; i++ {
-		m := Message{Priority: Priority(i << 3)}
-		_, err := m.ReadFrom(stream)
-		c.Assert(err, Not(IsNil))
-	}
-}
+	It("rejects invalid streams 2", func() {
+		stream := bytes.NewBufferString(`0 <0>1 2003-08-24T05:14:15.000003-07:00 - - - - -`)
+		for i := 0; i < 4; i++ {
+			m := rfc5424.Message{Priority: rfc5424.Priority(i << 3)}
+			_, err := m.ReadFrom(stream)
+			Expect(err).NotTo(BeNil())
+		}
+	})
+})
