524695e21ce468ee6590e318313622cb265498c5
Joris Baum <joris.baum@sap.com>
2026-03-05 13:48:02 +0100

    Bring back NewIngressTLSConfig and Egress func

--- ingress_client_test.go
+++ ingress_client_test.go
@@ -481,7 +481,7 @@ func getEnvelopeAt(receivers chan loggregator_v2.Ingress_BatchSenderServer, idx 
 }
 
 func buildIngressClient(serverAddr string, flushInterval time.Duration, addContext bool, certs *testhelper.TestCerts) (*loggregator.IngressClient, func()) {
-	tlsConfig, err := testhelper.NewIngressTLSConfig(
+	tlsConfig, err := loggregator.NewEgressTLSConfig(
 		certs.CA(),
 		certs.Cert("metron"),
 		certs.Key("metron"),
