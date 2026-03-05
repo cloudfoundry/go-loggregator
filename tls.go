b9e504bc4d939fbc0999f987f246a3aba479150c
Joris Baum <joris.baum@sap.com>
2026-03-05 13:48:02 +0100

    Bring back NewIngressTLSConfig and Egress func

--- tls.go
+++ tls.go
@@ -1,4 +1,4 @@
-package testhelper
+package loggregator
 
 import (
 	"crypto/tls"
@@ -9,6 +9,12 @@ import (
 // NewIngressTLSConfig provides a convenient means for creating a *tls.Config
 // which uses the CA, cert, and key for the ingress endpoint.
 func NewIngressTLSConfig(caPath, certPath, keyPath string) (*tls.Config, error) {
+	return newTLSConfig(caPath, certPath, keyPath, "metron")
+}
+
+// NewEgressTLSConfig provides a convenient means for creating a *tls.Config
+// which uses the CA, cert, and key for the egress endpoint.
+func NewEgressTLSConfig(caPath, certPath, keyPath string) (*tls.Config, error) {
 	return newTLSConfig(caPath, certPath, keyPath, "reverselogproxy")
 }
 
